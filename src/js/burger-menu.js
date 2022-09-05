3b4d50c04c3a540a37f8d1dfe6db0fadecf36a6d
DanielDondo <104907777+DanielDondo@users.noreply.github.com>
2022-09-05 17:18:52 +0300

    new start

---
+++ src/js/burger-menu.js
@@ -0,0 +1,11 @@
+(() => {
+    const iconMenu = document.querySelector('.header-burger__icon');
+    const menuBody = document.querySelector('.header-burger__body');
+    if (iconMenu) {
+        iconMenu.addEventListener("click", function (e) {
+            document.body.classList.toggle(`_lock`);
+            iconMenu.classList.toggle(`_active`);
+            menuBody.classList.toggle(`_active`);
+        });
+    }
+})();
