27b67762948e75e5b4c207ae1be3606e31c4a111
DanielDondo <104907777+DanielDondo@users.noreply.github.com>
2022-09-07 13:50:39 +0300

    new start

--- src/js/modal.js
+++ src/js/modal.js
@@ -16,6 +16,23 @@
 
 (() => {
     const refs = {
+        openModalBtn: document.querySelector("[data-buy-open-2]"),
+        closeModalBtn: document.querySelector("[data-buy-close-2]"),
+        modal: document.querySelector("[data-buy-2]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+        document.body.classList.toggle("modal-open");
+    }
+})();
+// ==========================================================================
+
+(() => {
+    const refs = {
         openModalBtn: document.querySelector("[data-tradition-open]"),
         closeModalBtn: document.querySelector("[data-tradition-close]"),
         modal: document.querySelector("[data-tradition]"),
@@ -61,12 +78,58 @@
         document.body.classList.toggle("modal-open");
     }
 })();
+// ==========================================================================
 
 (() => {
     const refs = {
         openModalBtn: document.querySelector("[data-product-open]"),
         closeModalBtn: document.querySelector("[data-product-close]"),
         modal: document.querySelector("[data-product]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+        document.body.classList.toggle("modal-open");
+    }
+})();
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-product-open-1]"),
+        closeModalBtn: document.querySelector("[data-product-close-1]"),
+        modal: document.querySelector("[data-product-1]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+        document.body.classList.toggle("modal-open");
+    }
+})();
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-product-open-2]"),
+        closeModalBtn: document.querySelector("[data-product-close-2]"),
+        modal: document.querySelector("[data-product-2]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+        document.body.classList.toggle("modal-open");
+    }
+})();
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-product-open-3]"),
+        closeModalBtn: document.querySelector("[data-product-close-3]"),
+        modal: document.querySelector("[data-product-3]"),
     };
 
     refs.openModalBtn.addEventListener("click", toggleModal);
