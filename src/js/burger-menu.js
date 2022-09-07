afdd8e916e20b23a0b088c227fd91ecf7a0c519e
DanielDondo <104907777+DanielDondo@users.noreply.github.com>
2022-09-07 15:59:24 +0300

    new start

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -16,3 +16,15 @@
         }
     });
 })();
+
+function backToTop() {
+    let button = $('.back-to-top');
+    $(window).on('scroll', () => {
+        if ($(this).scrollTop() >= 50) {
+            button.fadeIn();
+        } else {
+            button.fadeOut();
+        }
+    });
+}
+backToTop();
