839a3ebd50c38433c500f294c10b8a84af49484e
DanielDondo <104907777+DanielDondo@users.noreply.github.com>
2022-09-07 19:05:11 +0300

    start

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 import './js/burger-menu';
 import './js/modal';
 import './js/swiper-reviews';
-
-// if (window.jQuery) {
-//     console.log(`work`);
-// } else {
-//     console.log(`work-1`);
-// }
--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -16,15 +16,3 @@
         }
     });
 })();
-
-// function backToTop() {
-//     let button = $('.back-to-top');
-//     $(window).on('scroll', () => {
-//         if ($(this).scrollTop() >= 50) {
-//             button.fadeIn();
-//         } else {
-//             button.fadeOut();
-//         }
-//     });
-// }
-// backToTop();
